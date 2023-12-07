75f425345ffb1f9d438a75c1d4fa47f35e9d0110
Anre Borges-Brown <a.borgesbrown@gmail.com>
2023-12-07 16:38:46 -0600

    MS

--- js/keys.js
+++
@@ -1,5 +0,0 @@
-export const keys = {
-    github: "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",
-    mapbox: "pk.eyJ1IjoibWVycmVsbGoiLCJhIjoiY2xkNG5yY3NrMDc3djN2bzRzNHVkMHhxbiJ9.c-ih7Upq0rVd2rtEbMsDfw",
-    openweather: "9e4d47cd06e0eea6833746dc0183658f",
-}
